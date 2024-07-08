f0fc4217e05677cc84bec86176b0add914b4ffe5
NashIvan <nashivanramos@gmail.com>
2024-07-08 14:33:27 +0800

    Update buyer_product.php

--- buyer_product.php
+++ buyer_product.php
@@ -12,7 +12,7 @@ if(!isset($_GET['product_id'])){
             document.getElementById("time").textContent = countdown;
             if (countdown <= 0) {
                 clearInterval(intervalId);
-                location.href = "no_account.php";
+                location.href = "buyer.php";
             }
         }, 1000);
     </script>';
@@ -20,25 +20,49 @@ if(!isset($_GET['product_id'])){
     exit();
 }
 
-$ID = $_GET['product_id'];
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        echo '<html><body>';
+        echo '<div id="countdown">5</div>';
+        echo '<div id="message">Please login with a valid account first.</div>';
+        echo '<script>
+            var countdownElement = document.getElementById("countdown");
+            var messageElement = document.getElementById("message");
+            var countdown = 5;
+            var intervalId = setInterval(function() {
+                countdown--;
+                countdownElement.textContent = countdown;
+                if (countdown <= 0) {
+                    clearInterval(intervalId);
+                    window.location.href = "no_account.php";
+                }
+            }, 1000);
+        </script>';
+        echo '</body></html>';
+        exit;
+    }
+    
+    $ID = $_GET['product_id'];
+
 
     $specific_product = getProductbyId($ID);
     if(isset($_POST['add'])){
-        $query ="INSERT INTO buyer_db (FILE, ITEM_DESC, PRICE, CATEGORY, GENDER, Ptype)
-                SELECT FILE, ITEM_DESC, PRICE, CATEGORY, GENDER, Ptype
+        $USER_ID = $_SESSION['USER_ID'];
+        $query ="INSERT INTO buyer_db (FILE, ITEM_DESC, PRICE, CATEGORY, GENDER, Ptype, USER_ID)
+                SELECT FILE, ITEM_DESC, PRICE, CATEGORY, GENDER, Ptype, ?
                 FROM products
                 WHERE ID = ?";
         $stmt = mysqli_prepare($con, $query);
-        mysqli_stmt_bind_param($stmt, 'i', $ID);
+        mysqli_stmt_bind_param($stmt, 'ii', $USER_ID, $ID); // Bind USER_ID and ID
         mysqli_stmt_execute($stmt);
-
+    
         // Handle success or error
         if (mysqli_stmt_affected_rows($stmt) > 0) {
             echo "Product added to cart successfully!";
         } else {
             echo "Error adding product to cart.";
         }
-
+    
         mysqli_stmt_close($stmt);
         mysqli_close($con);
     }
@@ -62,7 +86,65 @@ $ID = $_GET['product_id'];
     </style>
 </head>
 <body>
-<h2>Online Shopping System</h2>
+
+<div class = "header">
+    <div style = "width: 900px;">
+        <p style = "margin-left: 75px; font-size: 25px;margin-top: 30px;color: #465a27; font-weight: bold;font-family: Times;">Online Shopping System<p>
+    </div>
+    <div style = "width: 300px">
+        <a href="cart.php"><button class = "menu-button">CART</button></a>
+        <form method="post">
+            <button name="logout" class="menu-button" onclick="return confirm('Are you sure you want to logout?');">LOGOUT</button>
+        </form>
+    </div>
+    </div>
+    <div class = "subheader">
+    <div style = "padding-right: 600px; display: inline-block;">
+        <p class = "subheading" style = "display: inline-block;">Welcome, <?php echo $_SESSION['username'];?>!</p>
+    </div>  
+    <!-- Trigger the modal with a button -->
+    </div>
+    <div style = "height: 1px; background-color: black;margin-left:230px;width:949.6px;margin-bottom:30px;"></div>
+
+<div class="sidebar-category">
+    <div class = "Home">
+        <a href="buyer.php"><h3>Main</h3></a>
+    </div>
+        <a href=""></a>
+        <h3 class = "sidebar-header">Apparels</h3>
+    <div class="apparels">
+            <div class="male">
+                <h4 class = "sidebar-subheader">Male</h4>
+                <a href="buyer_category.php"><p>Brief</p></a>
+                <a href="buyer_category.php?type=Jackets&gender=male"><p>Jackets</p></a>
+                <a href="buyer_category.php?type=Jeans&gender=male"><p>Jeans</p></a>
+                <a href="buyer_category.php?type=T-Shirt&gender=male"><p>T-shirt</p></a>
+            </div>
+            <div class="female">
+                <h4 class = "sidebar-subheader">Female</h4>
+                <a href="buyer_category.php?type=Blouse&gender=female"><p>Blouse</p></a> 
+                <a href="buyer_category.php?type=Dress&gender=female"><p>Dress</p></a> 
+                <a href="buyer_category.php?type=<?php echo urlencode('Maxi Dresses'); ?>&gender=female"><p>Maxi Dresses</p></a> 
+                <a href="buyer_category.php?type=Cardigans&gender=female"><p>Cardigans</p></a> 
+            </div>
+    </div>
+    <h3 class = "sidebar-header">Accessories</h3>
+    <div class="accessory">
+            <div class="male">
+                <h4 class = "sidebar-subheader">Male</h4>
+                <a href="buyer_category.php?type=Belts&gender=male"><p>Belts</p></a>
+                <a href="buyer_category.php?type=Sunglasses&gender=male"><p>Sunglasses</p></a>
+                <a href="buyer_category.php?type=Watch&gender=male"><p>Watch</p></a>
+            </div>
+            <div class="female">
+                <h4 class = "sidebar-subheader">Female</h4>
+                <a href="buyer_category.php?type=Belts&gender=female"><p>Belts</p></a> 
+                <a href="buyer_category.php?type=Necklace&gender=female"><p>Necklace</p></a>
+                <a href="buyer_category.php?type=Sunglasses&gender=female"><p>Sunglasses</p></a>
+            </div>
+    </div>
+    </div>
+
 <div class="container">
     <div class="display-items">
         <!-- Call our functions here -->
@@ -78,6 +160,9 @@ $ID = $_GET['product_id'];
                     <?php echo $specific_product['ITEM_DESC']; ?>
                 </p>
             </div>
+            <div class="item-price">
+                <p class="Price">$<?php echo $specific_product['PRICE']; ?></p>
+            </div>
             <div class="item-category">
                 <p class="category"><?php echo $specific_product['CATEGORY']; ?></p>
             </div>
@@ -87,8 +172,8 @@ $ID = $_GET['product_id'];
             <div class="item-type">
                 <p class="Ptype"><?php echo $specific_product['Ptype']; ?></p>
             </div>
-            <div class="item-type">
-                <p class="Price">$<?php echo $specific_product['PRICE']; ?></p>
+            <div class="MORE_DESC">
+                <p class="MORE_DESC"><?php echo $specific_product['MORE_DESC']; ?></p>
             </div>
             <?php
         } else {
@@ -118,17 +203,16 @@ $ID = $_GET['product_id'];
                         </div>
                         <div class="item-details">
                             <p class="title">
-                                <a href="product.php?product_id=<?php echo $product['ID']; ?>" target = "blank"><?php echo $product['ITEM_DESC']?></a>
+                                <a href="buyer_product.php?product_id=<?php echo $product['ID']; ?>" target = "blank"><?php echo $product['ITEM_DESC']?></a>
                             </p>
+                            <p class="price">$<?php echo $product['PRICE']?></p>  
                             <p class="category"><?php echo $product['CATEGORY']?></p> 
                             <p class="Ptype"><?php echo $product['Ptype']?></p> 
-                            <p class="price">$<?php echo $product['PRICE']?></p>  
                         </div>
                     </div>  
                     <?php
                 }
-                ?>
-            
+                ?>     
     </div>
 </div>
 </body>
